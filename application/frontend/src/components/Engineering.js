fb948f7cb6a64c1b83069115f0ba02e1e89e8998
Ian <47256454+IanRFerguson@users.noreply.github.com>
2024-06-29 15:59:22 -0400

    wip

--- application/frontend/src/components/Engineering.js
+++ application/frontend/src/components/Engineering.js
@@ -1,56 +1,118 @@
+import Carousel from "react-multi-carousel"
+import "react-multi-carousel/lib/styles.css";
+
+import nba_dbt from "../media/nba_dbt.png"
+import klondike_sf from "../media/klondike_sf.jpeg"
+import nba_ppg from "../media/nba_ppg.png"
+import glmx from "../media/glmx.png"
+
+///
+
+const Portfolio = (props) => {
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            slidesToSlide: 1 // optional, default to 1.
+        }
+    };
+
+    const carousel_display = <Carousel
+        swipeable={false}
+        draggable={false}
+        showDots={true}
+        responsive={responsive}
+        ssr={true} // means to render carousel on server-side.
+        infinite={true}
+        autoPlay={true}
+        autoPlaySpeed={20000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={5000}
+        containerClass="carousel-container"
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px"
+    >
+        <div className="engineeringCarouselItem">
+            <h3>Knicks Player Analytics</h3>
+            <p>
+                <a href="https://github.com/IanRFerguson/player-analytics" target="_blank">
+                    View it on Github
+                </a>
+            </p>
+            <p>
+                A full-scale ELT pipeline that ingests data from the NBA API into
+                BigQuery using a custom Python client. The raw data is then transformed
+                using into player summary models using dbt. All of this is orchestrated in
+                Prefect, and the cloud resources are managed in version control with Terraform.
+            </p>
+            <img src={nba_dbt} className="engineeringCarouselImage"></img>
+        </div>
+        <div className="engineeringCarouselItem">
+            <h3>Klondike</h3>
+            <p>
+                <a href="https://github.com/IanRFerguson/klondike" target="_blank">
+                    View it on Github
+                </a>
+            </p>
+            <p>
+                Klondike offers a suite of database connectors powered by Polars. These tools are
+                optimized for efficiency using the Rust-speedups native to Polars in conjunction with
+                PyArrow for serialized data loading. This is a "just for fun" exercise, but has snuck
+                its way into several professional project where scale was a meaningful factor.
+            </p>
+            <img src={klondike_sf} className="engineeringCarouselImage"></img>
+        </div>
+        <div className="engineeringCarouselItem">
+            <h3>GLM Express</h3>
+            <p>
+                <a href="https://github.com/IanRFerguson/glm-express/tree/main" target="_blank">
+                    View it on Github
+                </a>
+            </p>
+            <p>
+                GLM Express is an automated solution to modeling subject- and group-level
+                neuroimaging data. All you need is a valid BIDS project and you can fully customize
+                your contrasts, output types, and visualizations. One of my first engineering projects!
+            </p>
+            <img src={glmx} className="engineeringCarouselImage"></img>
+        </div>
+        <div className="engineeringCarouselItem">
+            <h3>NBA PPG Full-Stack Application</h3>
+            <p>
+                <a href="https://github.com/IanRFerguson/nba-ppg" target="_blank">
+                    View it on Github
+                </a>
+            </p>
+            <p>
+                Are you picking up on a theme? This is a full-stack web application that
+                extracts and loads data from the NBA API, minimally transforms it, and presents
+                it to the end user in the form a D3.js interactive chart.
+            </p>
+            <img src={nba_ppg} className="engineeringCarouselImage"></img>
+        </div>
+    </Carousel>;
+
+    return carousel_display
+}
+
 const Engineering = (props) => {
     return (
         <div className="sectionDisplay">
             <div className="sectionHeader">
                 <h1>Engineering Portfolio</h1>
             </div>
-            <div className="engineeringContent">
-                {/* Player Analytics */}
-                <div className="engineeringBlock">
-                    <h3>Knicks Player Analytics</h3>
-                    <ul>
-                        <li>Raw player data is extracted from the NBA API and loaded into BigQuery using a custom Python connector</li>
-                        <li>Using dbt, data is quality tested and modeled into useful summary metrics</li>
-                        <li>The source code is hosted in a Docker image, managed in the cloud with Terraform, and orchestrated with Prefect</li>
-                    </ul>
-                    <p>
-                        <a href="https://github.com/IanRFerguson/player-analytics" target="_blank">
-                            View it on Github
-                        </a>
-                    </p>
-                </div>
-
-                {/* Klondike */}
-                <div className="engineeringBlock">
-                    <h3>Klondike</h3>
-                    <p>
-                        Klondike offers a suite of database connectors powered by Polars. These tools are
-                        optimized for efficiency using the Rust-speedups native to Polars in conjunction with
-                        PyArrow for serialized data loading. This is a "just for fun" exercise, but has snuck
-                        its way into several professional project where scale was a meaningful factor.
-                    </p>
-                    <p>
-                        <a href="https://github.com/IanRFerguson/klondike" target="_blank">
-                            View it on Github
-                        </a>
-                    </p>
-                </div>
-
-                {/* NBA PPG */}
-                <div className="engineeringBlock">
-                    <h3>NBA PPG Full-Stack Application</h3>
-                    <p>
-                        Are you picking up on a theme? This is a full-stack web application that
-                        extracts and loads data from the NBA API, minimally transforms it, and presents
-                        it to the end user in the form a D3.js interactive chart.
-                    </p>
-                    <p>
-                        <a href="https://github.com/IanRFerguson/nba-ppg" target="_blank">
-                            View it on Github
-                        </a>
-                    </p>
-                </div>
-            </div>
+            <Portfolio />
         </div>
     )
 }

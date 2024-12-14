d33769d16293fe25684cfbb17b732de91a096cad
Ian <47256454+IanRFerguson@users.noreply.github.com>
2024-12-14 07:52:58 -0500

    wip

--- application/frontend/src/components/About.js
+++ application/frontend/src/components/About.js
@@ -4,31 +4,24 @@ const About = (props) => {
             <div className="sectionHeader">
                 <h1>About Me</h1>
             </div>
-            <div className="AIATT">
-                {/* Brief autobiography */}
-                <div className="aboutMeBox">
-                    <p></p>
+            <div className="aboutContent">
+                <div className="aboutParagraph">
+                    I'm a data engineer and full-stack technologist based in New York City.
                 </div>
-
-                {/* Nonprofit tech */}
-                <div className="aboutMeBox">
-                    <p>
-                        I transitioned into nonprofit tech in the summer of 2022 with the goal of
-                        leveraging my skill set to progress social causes I care about. Since then,
-                        I have worked as an engineer at organizations such as The Movement Cooperative and
-                        Movement Labs where I develop automated software and data solutions to supercharge
-                        the tech infrastructure of the progressive left. For a broad overview of my job experience
-                        and technical expertise, please <a href="https://drive.google.com/file/d/1z3EP3uJBIPxLH6DTsqZgpmlKmkYLP-CO/view?usp=sharing">see my resume</a>.
-                    </p>
+                <div className="aboutParagraph">
+                    Prior to finding my footing in data engineering, I pursued a career as a research psychologist where I focused on quantifying ambient predicates of social concervative shift.
+                    I earned my Masters in Psychology with a concentration in Social Neuroscience from New York University in 2020, where I studied with <a href="https://www.jonbfreeman.com" target="_blank">Dr. Jon Freeman</a> in the <a href="https://www.freemanlab.org" target="_blank">Social Cognitive and Neural Sciences Lab</a> as a Graduate Research Assistant.
+                    From 2020 to 2022 I served as the Lab Manager and Research Engineer for Stanford University's <a href="https://www.ssnl.stanford.edu" target="_blank">Social Neuroscience Lab</a> under the direction of <a href="https://www.jamil-zaki.com" target="_blank">Dr. Jamil Zaki</a>.
                 </div>
-
-                {/* Outside of tech */}
-                <div className="aboutMeBox">
-                    <p></p>
+                <div className="aboutParagraph">
+                    <ul>
+                        <li><b><i><a href="https://drive.google.com/file/d/1L7G3iU2ldK4k4XYQ6jvOPC88zT1LMdg6/view?usp=sharing" target="_blank">Assessing Predictors of Threat Perception in Mixed-Race Groups</a></i></b></li>
+                        <li><b><i><a href="https://www.ruipei.net/files/papers/ufrVoPGSRksC.pdf" target="_blank">A neural signature of social support mitigates negative emotion</a></li>
+                        </ul>
+                        </div>
                 </div>
             </div>
-        </div>
-    )
+            )
 }
 
-export default About;
+            export default About;
--- application/frontend/src/components/App.js
+++ application/frontend/src/components/App.js
@@ -1,6 +1,7 @@
 import '../App.css';
 import '../App-mobile.css'
 import Landing from './Landing.js';
+import About from './About.js';
 import Engineering from './Engineering.js';
 import Contact from './Contact.js';
 import Footer from './Footer';
@@ -9,7 +10,7 @@ function IanIsOnline() {
   return (
     <div className="App">
       <Landing />
-      <Engineering />
+      <About />
       <Contact />
       <Footer />
     </div>
